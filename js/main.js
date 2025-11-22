4c8bb56d2e5c9ff117caac43c5a5fcca571c4f81
Joseph Murillo <josephmurillo1621@gmail.com>
2025-11-21 21:50:29 -0600

    (feature):  project cards

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ document.getElementById("year").textContent = new Date().getFullYear();
 // contact form
 document.getElementById("contact-form").addEventListener("submit", function(e){
   e.preventDefault();
-
   const status = document.getElementById("form-status");
   status.textContent = "Message sent successfully!";
   status.style.color = "#00ff9d";
-
   this.reset();
+});
+
+function openModal(id) {
+  document.getElementById(id).style.display = "flex";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+window.addEventListener("click", function(e) {
+  document.querySelectorAll(".modal-freelancer").forEach(modal => {
+    if (e.target === modal) modal.style.display = "none";
+  });
 });
